ba072ccd3c4fcbb33f0339f5c7ef76e580721eb2
Aadit Kachalia <aaditkachalia@gmail.com>
2020-05-29 14:48:08 +0530

    Debug

--- Ezhire-Backend/app.py
+++ Ezhire-Backend/app.py
@@ -648,4 +648,4 @@ def register():
 # 	return json.dumps(final_interview)   
 
 if __name__ == '__main__':
-	socketio.run(app,port=5000,host= '0.0.0.0')
+	socketio.run(app,port=5000,host= '0.0.0.0',debug=True)
